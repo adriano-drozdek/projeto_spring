9308a2562ba323be17c49e2a3d7a61108f9d939e
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-04-21 16:30:58 -0300

    @RequestMapping criado para fazer o mapeamento de rede ex: http://localhost:8080/categoria

--- meuprimeiroprojeto/src/main/java/com/adrianodrozdek/meuprimeiroprojeto/recursos/RecursoDaCategoria.java
+++ meuprimeiroprojeto/src/main/java/com/adrianodrozdek/meuprimeiroprojeto/recursos/RecursoDaCategoria.java
@@ -1,9 +1,10 @@
 package com.adrianodrozdek.meuprimeiroprojeto.recursos;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping(value = "/categoria")
 public class RecursoDaCategoria {
 
 }
