20668e5acc9abf5920c08eb6be7f8e971112ae1b
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-04-21 16:28:47 -0300

    @RestController criado para o spring boot reconhecer como recurso rest

--- meuprimeiroprojeto/src/main/java/com/adrianodrozdek/meuprimeiroprojeto/recursos/RecursoDaCategoria.java
+++ meuprimeiroprojeto/src/main/java/com/adrianodrozdek/meuprimeiroprojeto/recursos/RecursoDaCategoria.java
@@ -1,5 +1,9 @@
 package com.adrianodrozdek.meuprimeiroprojeto.recursos;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
 public class RecursoDaCategoria {
 
 }
