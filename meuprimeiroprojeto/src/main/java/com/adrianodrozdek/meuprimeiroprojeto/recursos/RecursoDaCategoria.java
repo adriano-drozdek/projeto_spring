79ef7f28d5424599cbc202a310e451b3dce9bbef
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-04-21 16:39:39 -0300

    Assinatura do método para retornar todas as categorias

--- meuprimeiroprojeto/src/main/java/com/adrianodrozdek/meuprimeiroprojeto/recursos/RecursoDaCategoria.java
+++ meuprimeiroprojeto/src/main/java/com/adrianodrozdek/meuprimeiroprojeto/recursos/RecursoDaCategoria.java
@@ -1,10 +1,21 @@
 package com.adrianodrozdek.meuprimeiroprojeto.recursos;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.adrianodrozdek.meuprimeiroprojeto.entities.Categoria;
 
 @RestController
 @RequestMapping(value = "/categoria")
 public class RecursoDaCategoria {
 
+	@GetMapping
+	public ResponseEntity<List<Categoria>> findAll(){
+		
+	}
 }
