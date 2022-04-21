f36dd28b671e73851849473d8d25fd3c71130919
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-04-21 17:02:58 -0300

    Implementando busca por {id}

--- meuprimeiroprojeto/src/main/java/com/adrianodrozdek/meuprimeiroprojeto/recursos/RecursoDaCategoria.java
+++ meuprimeiroprojeto/src/main/java/com/adrianodrozdek/meuprimeiroprojeto/recursos/RecursoDaCategoria.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +18,14 @@ public class RecursoDaCategoria {
 	@GetMapping
 	public ResponseEntity<List<Categoria>> findAll() {
 		List<Categoria> list = new ArrayList<>();
-		list.add(new Categoria(1l, "Eletronicos"));
+		list.add(new Categoria(1L, "Eletronicos"));
 		list.add(new Categoria(2L, "Caderno"));
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
+	public ResponseEntity<Categoria> findById(@PathVariable Long id) {
+		Categoria cat = new Categoria(1L, "Celular");
+		return ResponseEntity.ok().body(cat);
+	}
 }
